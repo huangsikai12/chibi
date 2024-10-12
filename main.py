007052c972b317a08f9625f5e17e54baa83023c7
huangsikai12 <huangsikai6666@gmail.com>
2024-10-12 18:27:50 +0800

    add-抢课文件

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import Constants
 from DataAction import getHd
 
-
+# Designed by 黄斯恺
 def setData():
     Constants.JSESSIONID = input("请输入 JSESSIONID: ")
     Constants.HD_ID = input("请输入要抢的活动ID：")
