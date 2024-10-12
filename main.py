38fec744d3a93e58a6b6c50ca6d6271425f1bf5d
huangsikai12 <huangsikai6666@gmail.com>
2024-10-12 18:29:42 +0800

    add-抢课文件

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ import sys
 import Constants
 from DataAction import getHd
 
+
 # Designed by 黄斯恺
 def setData():
     Constants.JSESSIONID = input("请输入 JSESSIONID: ")
     Constants.HD_ID = input("请输入要抢的活动ID：")
 
+
 if __name__ == "__main__":
+    print("欢迎使用上海商学院第二课堂抢课脚本！请在使用之前阅读README文档~！")
     try:
         setData()
     except:
